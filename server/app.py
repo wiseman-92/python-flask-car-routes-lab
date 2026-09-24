91b365dd391aa1fcb0ee37d0fe43f5eb8f8f2156
Branton Kipruto Birgen <brantonbirgen@gmail.com>
2026-09-24 10:44:54 +0300

    last commit

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def default():
+    return "Welcome to Flatiron Cars"
+
+@app.route('/<model>')
+def dynamic(model):
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    else:
+        return f'No models called {model} exists in our catalog'
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
